36916e3e23250627a6fa490e3b349d5adb9ed636
R. Biojout <rbiojout@gmail.com>
2016-05-26 21:12:00 +0200

    Changed the WYSIWYG Editor

--- config/application.rb
+++ config/application.rb
@@ -13,8 +13,8 @@ module Formycourse
     # -- all .rb files in that directory are automatically loaded.
 
     # add the fonts to assets
-    config.assets.paths << Rails.root.join("app", "assets", "fonts")
-
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
+    
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
