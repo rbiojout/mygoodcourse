30f18159e2cd99421e610c1d58223e3530e92fff
R. Biojout <rbiojout@gmail.com>
2016-06-20 22:11:47 +0200

    Added i18n for devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,8 +62,18 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'https://ancient-chamber-37263.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :address        => 'smtp.gmail.com',
+      :port           => '587',
+      :authentication => :plain,
+      :user_name      => ENV['GMAIL_USERNAME'],
+      :password       => ENV['GMAIL_PASSWORD'],
+      :enable_starttls_auto => true
+  }
 
-  config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
