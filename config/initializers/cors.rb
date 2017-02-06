80f4b14b4cc712c3d2ec9c5b05c7c9769c8c071a
R. Biojout <rbiojout@gmail.com>
2017-02-06 16:03:58 +0100

    Regex for cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,14 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'localhost:3000', '127.0.0.1:3000',
+            /https?:\/\/(.*).cloudfront.net/
+    # regular expressions can be used here
 
     resource '/assets/*',
+             headers: :any,
+             methods: [:get]
+    resource '/webpack/*',
              headers: :any,
              methods: [:get]
   end
