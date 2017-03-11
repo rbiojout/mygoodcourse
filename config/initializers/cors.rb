bb8f4f0fc3cf0775ac57a8fe4dc695233cf8a93b
rb <rbiojout@gmail.com>
2017-03-11 08:38:31 +0100

    changed to Webpack 2 and added the node and npm versions to package.json

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,7 +4,17 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
 
   allow do
-    origins '*'
+    origins %w[
+                http://localhost:3000,
+                http://127.0.0.1:3000,
+                https://mygoodcourse.herokuapp.com,
+                https://mygoodcourse-staging.herokuapp.com,
+                https://www.mygoodcourse.com,
+                https://djydktkci79tz.cloudfront.net,
+                https://d1b6d82jm5evkl.cloudfront.net,
+                http://djydktkci79tz.cloudfront.net,
+                http://d1b6d82jm5evkl.cloudfront.net
+            ]
     # regular expressions can be used here
 
     resource '*', :headers => :any, :methods => :any
